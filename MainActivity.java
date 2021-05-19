a73a25ea8a5a787964fbc81c3ed9960523088ac1
jv232 <jaganv00@outlook.com>
2021-05-19 10:00:21 -0400

    code completed

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.miniproject;
 
-import java.io.*;
-import java.util.ArrayList;
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.*;
-import android.widget.AdapterView.OnItemClickListener;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 
@@ -15,9 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     public static final String TAG = "File:";
     private String[] textList;
-    //private EditText editText;
-    private ArrayList<String> list = new ArrayList<>();
-    private ArrayList<String> textLists;
+    private ArrayAdapter<String> arrayAdapter;
     private static boolean firstPass = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,66 +23,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(getClass().getSimpleName(), "onCreate()");
 
+        //to avoid rewriting entire information in this activity while switching between activities
         if (firstPass){
-            Log.d(TAG, "first is true, setting to false");
             firstPass=false;
-            textList = ListTitle.getArray();
+
         }
         else{
             Log.d(TAG, "firstpass is:" + firstPass);
             Bundle extras = getIntent().getExtras();
-
+            //Gets the position from the ListTitle
             pos = ListTitle.getPosition();
+            //gets title information from NoteActivity
             str = getIntent().getStringExtra("title");
+            //updates the array in ListTitle
             ListTitle.setArray(pos, str);
-            textList = ListTitle.getArray();
-            Log.d(TAG, "firstpass is:" +textList[pos]);
         }
+        //gets array from a normal class ListTitle
+        textList = ListTitle.getArray();
 
-        File[] files;
-        FileWriter fw = null;
-        BufferedWriter bw = null;
-        PrintWriter pw = null;
-        Context context = getApplicationContext();
-        Log.d(TAG,"context.getFilesDir() is: "+context.getFilesDir());
-        String path = context.getFilesDir().getPath();
-        Log.d(TAG,"String path = context.getFilesDir().getPath() is :"+path);
-        for (int i =0; i < 12; i++){
-            File file = new File (path+"/note"+i+".txt");
 
-            try {
-                fw = new FileWriter(file, true);
-                bw = new BufferedWriter(fw);
-                pw = new PrintWriter(bw);
-
-                pw.println("");
-                //don't forget to clean up
-                pw.flush();
-                pw.close();
-                bw.close();
-                fw.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
         listView = findViewById(R.id.listView);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>
+         arrayAdapter = new ArrayAdapter<>
                 (this, android.R.layout.simple_list_item_1,
                         textList);
         listView.setAdapter(arrayAdapter);
         //set the callbacks for the listView
         listView.setClickable(true);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long rowID) {
                 Object obj = listView.getItemAtPosition(position);
                 Toast toast = Toast.makeText(MainActivity.this, obj.toString(), Toast.LENGTH_SHORT);
                 toast.show();
+                //Could have used putExtra but felt it may get too complicated hence use ListTitle to store position information
                 ListTitle.setPosition(position);
                 Intent noteIntent = new Intent(MainActivity.this, NoteActivity.class);
-
                 startActivity(noteIntent);
+                arrayAdapter.notifyDataSetChanged();
             }
         });
 
